c12d2314d0ec3ee5accd149e5906f54b0463762a
ZhangGuohua <zhangguohua@yunintegration.com>
2017-09-03 22:04:15 +0800

    视频中检测对象

--- src/main/java/andy/javacv/ObjectDetectionInMonitor.java
+++ src/main/java/andy/javacv/ObjectDetectionInMonitor.java
@@ -3,23 +3,40 @@ package andy.javacv;
 /**
  * Created by ZhangGuohua on 2017/9/3.
  */
+
 import java.io.File;
-import java.net.URL;
+
 import org.bytedeco.javacv.*;
 import org.bytedeco.javacpp.*;
 import org.bytedeco.javacpp.indexer.*;
+
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 import static org.bytedeco.javacpp.opencv_calib3d.*;
 import static org.bytedeco.javacpp.opencv_objdetect.*;
-public class Demo {
+
+/**
+ * 2017-09-03
+ * 例子来源 : https://github.com/andy-zhang-guohua/javacv Demo，
+ * 问题1 : Exception in thread "main" org.bytedeco.javacv.FrameGrabber$Exception: videoInput.getPixels() Error: Could not get pixels.
+ * at org.bytedeco.javacv.VideoInputFrameGrabber.grab(VideoInputFrameGrabber.java:171)
+ * at andy.javacv.ObjectDetectionInMonitor.main(ObjectDetectionInMonitor.java:55)
+ * 问题1 方案 :
+ * 来源 ： https://github.com/sagioto/LipReading/issues/15
+ * grabber = VideoInputFrameGrabber.createDefault(0); 一句替换成 :
+ * grabber = new OpenCVFrameGrabber(0);
+ */
+public class ObjectDetectionInMonitor {
     public static void main(String[] args) throws Exception {
         String classifierName = null;
         if (args.length > 0) {
             classifierName = args[0];
         } else {
-            URL url = new URL("https://raw.github.com/Itseez/opencv/2.4.0/data/haarcascades/haarcascade_frontalface_alt.xml");
-            File file = Loader.extractResource(url, null, "classifier", ".xml");
+            // 网络可用的情况下可以使用这个方案
+            //URL url = new URL("https://raw.github.com/Itseez/opencv/2.4.0/data/haarcascades/haarcascade_frontalface_alt.xml");
+            //File file = Loader.extractResource(url, null, "classifier", ".xml");
+            // 如果没有网络支持，使用这种方案
+            File file = Loader.extractResource(ObjectDetectionInMonitor.class, "/Itseez/opencv/2.4.0/data/haarcascades/haarcascade_frontalface_alt.xml", null, "classifier", ".xml");
             file.deleteOnExit();
             classifierName = file.getAbsolutePath();
         }
@@ -37,7 +54,9 @@ public class Demo {
         // The available FrameGrabber classes include OpenCVFrameGrabber (opencv_videoio),
         // DC1394FrameGrabber, FlyCaptureFrameGrabber, OpenKinectFrameGrabber, OpenKinect2FrameGrabber,
         // RealSenseFrameGrabber, PS3EyeFrameGrabber, VideoInputFrameGrabber, and FFmpegFrameGrabber.
-        FrameGrabber grabber = FrameGrabber.createDefault(0);
+        //FrameGrabber grabber = VideoInputFrameGrabber.createDefault(0);
+        FrameGrabber grabber = new OpenCVFrameGrabber(0);
+        ;
         grabber.start();
 
         // CanvasFrame, FrameGrabber, and FrameRecorder use Frame objects to communicate image data.
@@ -53,9 +72,9 @@ public class Demo {
         // - Java2DFrameConverter also has static copy() methods that we can use to transfer
         //   data more directly between BufferedImage and IplImage or Mat via Frame objects.
         IplImage grabbedImage = converter.convert(grabber.grab());
-        int width  = grabbedImage.width();
+        int width = grabbedImage.width();
         int height = grabbedImage.height();
-        IplImage grayImage    = IplImage.create(width, height, IPL_DEPTH_8U, 1);
+        IplImage grayImage = IplImage.create(width, height, IPL_DEPTH_8U, 1);
         IplImage rotatedImage = grabbedImage.clone();
 
         // Objects allocated with a create*() or clone() factory method are automatically released
@@ -71,18 +90,20 @@ public class Demo {
         // CanvasFrame is a JFrame containing a Canvas component, which is hardware accelerated.
         // It can also switch into full-screen mode when called with a screenNumber.
         // We should also specify the relative monitor/camera response for proper gamma correction.
-        CanvasFrame frame = new CanvasFrame("Some Title", CanvasFrame.getDefaultGamma()/grabber.getGamma());
+        CanvasFrame frame = new CanvasFrame("视频监控对象识别", CanvasFrame.getDefaultGamma() / grabber.getGamma());
 
         // Let's create some random 3D rotation...
         CvMat randomR = CvMat.create(3, 3), randomAxis = CvMat.create(3, 1);
         // We can easily and efficiently access the elements of matrices and images
         // through an Indexer object with the set of get() and put() methods.
         DoubleIndexer Ridx = randomR.createIndexer(), axisIdx = randomAxis.createIndexer();
-        axisIdx.put(0, (Math.random()-0.5)/4, (Math.random()-0.5)/4, (Math.random()-0.5)/4);
+        axisIdx.put(0, (Math.random() - 0.5) / 4, (Math.random() - 0.5) / 4, (Math.random() - 0.5) / 4);
         cvRodrigues2(randomAxis, randomR, null);
-        double f = (width + height)/2.0;  Ridx.put(0, 2, Ridx.get(0, 2)*f);
-        Ridx.put(1, 2, Ridx.get(1, 2)*f);
-        Ridx.put(2, 0, Ridx.get(2, 0)/f); Ridx.put(2, 1, Ridx.get(2, 1)/f);
+        double f = (width + height) / 2.0;
+        Ridx.put(0, 2, Ridx.get(0, 2) * f);
+        Ridx.put(1, 2, Ridx.get(1, 2) * f);
+        Ridx.put(2, 0, Ridx.get(2, 0) / f);
+        Ridx.put(2, 1, Ridx.get(2, 1) / f);
         System.out.println(Ridx);
 
         // We can allocate native arrays using constructors taking an integer as argument.
@@ -93,18 +114,17 @@ public class Demo {
 
             // Let's try to detect some faces! but we need a grayscale image...
             cvCvtColor(grabbedImage, grayImage, CV_BGR2GRAY);
-            CvSeq faces = cvHaarDetectObjects(grayImage, classifier, storage,
-                    1.1, 3, CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_DO_ROUGH_SEARCH);
+            CvSeq faces = cvHaarDetectObjects(grayImage, classifier, storage, 1.1, 3, CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_DO_ROUGH_SEARCH);
             int total = faces.total();
             for (int i = 0; i < total; i++) {
                 CvRect r = new CvRect(cvGetSeqElem(faces, i));
                 int x = r.x(), y = r.y(), w = r.width(), h = r.height();
-                cvRectangle(grabbedImage, cvPoint(x, y), cvPoint(x+w, y+h), CvScalar.RED, 1, CV_AA, 0);
+                cvRectangle(grabbedImage, cvPoint(x, y), cvPoint(x + w, y + h), CvScalar.RED, 1, CV_AA, 0);
 
                 // To access or pass as argument the elements of a native array, call position() before.
-                hatPoints.position(0).x(x-w/10)   .y(y-h/10);
-                hatPoints.position(1).x(x+w*11/10).y(y-h/10);
-                hatPoints.position(2).x(x+w/2)    .y(y-h/2);
+                hatPoints.position(0).x(x - w / 10).y(y - h / 10);
+                hatPoints.position(1).x(x + w * 11 / 10).y(y - h / 10);
+                hatPoints.position(2).x(x + w / 2).y(y - h / 2);
                 cvFillConvexPoly(grabbedImage, hatPoints.position(0), 3, CvScalar.GREEN, CV_AA, 0);
             }
 
@@ -113,12 +133,11 @@ public class Demo {
 
             // To check if an output argument is null we may call either isNull() or equals(null).
             CvSeq contour = new CvSeq(null);
-            cvFindContours(grayImage, storage, contour, Loader.sizeof(CvContour.class),
-                    CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+            cvFindContours(grayImage, storage, contour, Loader.sizeof(CvContour.class), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
             while (contour != null && !contour.isNull()) {
                 if (contour.elem_size() > 0) {
                     CvSeq points = cvApproxPoly(contour, Loader.sizeof(CvContour.class),
-                            storage, CV_POLY_APPROX_DP, cvContourPerimeter(contour)*0.02, 0);
+                            storage, CV_POLY_APPROX_DP, cvContourPerimeter(contour) * 0.02, 0);
                     cvDrawContours(grabbedImage, points, CvScalar.BLUE, CvScalar.BLUE, -1, 1, CV_AA);
                 }
                 contour = contour.h_next();
